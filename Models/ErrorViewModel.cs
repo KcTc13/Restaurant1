ee49f349c1263704461e45c4f7fc4313be626cf7
kevyn <kevyn@DESKTOP-JPLIFBG>
2024-09-24 20:09:48 -0700

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Restaurant1.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
